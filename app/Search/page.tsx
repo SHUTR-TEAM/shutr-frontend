74e827edd264c786ba465385346f747038623692
Chamindu-Dharmawickrema <chamindudharmawickrema@gmail.com>
2024-12-17 00:01:13 +0530

    Create a Category Section

---
+++ app/Search/page.tsx
@@ -0,0 +1,11 @@
+import CategorySection from "../components/Search/CategorySection";
+import SearchBox from "../components/Search/SeacrhBox";
+
+
+  export default function Search() {
+    return (
+      <div>
+        <CategorySection />
+      </div>
+    );
+  }
