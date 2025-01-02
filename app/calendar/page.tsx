1826e45a6ef69e5df37635b3086c02eb8b323b59
Sashan Fernando <164195487+Sashan2002@users.noreply.github.com>
2025-01-02 19:41:09 +0530

    Create Calendar

---
+++ app/calendar/page.tsx
@@ -0,0 +1,12 @@
+import Calendar from "../components/Calendar/Calendar"
+
+function CalendarPage() {
+    return (
+      <>
+      <Calendar/>
+      </>
+     
+    )
+  }
+  
+  export default CalendarPage;
