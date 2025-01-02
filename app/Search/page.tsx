79301ad3f9354a730ab2b96404f83f488a183c90
Pamudi <tpamudirajapaksha@gmail.com>
2025-01-02 13:14:31 +0530

    Update the card, create new features, and ensure mobile responsiveness

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -1,70 +1,71 @@
 import CategorySection from "../components/Search/CategorySection";
 import SearchBox from "../components/Search/SeacrhBox";
-import SmallPortfolio from "../components/Search/SmallPortfolio";
+import Card from "../components/Search/Card";
 import styles from "./page.module.css";
 
 const data = [
-    {
-      id: 1,
-      image: '/pic.png',
+  {
+    id: 1,
+    images: ['/pic.png', '/pic.png', '/pic.png'],
+    name: 'John Doe',
+    price: 8000,
+    tags: ['Wedding', 'Portrait'],
+    location: 'Greenwich Court',
+    reviews: 8,
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    images: ['/pic.png', '/pic.png', '/pic.png'], 
+    name: 'John Doe',
+    price: 8000,
+    tags: ['Wedding', 'Portrait'],
+    location: 'Greenwich Court',
+    reviews: 8,
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    images: ['/pic.png', '/pic.png', '/pic.png'], 
+    name: 'John Doe',
+    price: 8000,
+    tags: ['Wedding', 'Portrait'],
+    location: 'Greenwich Court',
+    reviews: 8,
+    rating: 4.5,
+  },
+  {
+      id: 4,
+      images: ['/pic.png', '/pic.png', '/pic.png'], 
       name: 'John Doe',
       price: 8000,
       tags: ['Wedding', 'Portrait'],
       location: 'Greenwich Court',
       reviews: 8,
       rating: 4.5,
-    },
-    {
-      id: 2,
-      image: '/pic.png',
+  },
+  {
+      id: 5,
+      images: ['/pic.png', '/pic.png', '/pic.png'], 
       name: 'John Doe',
       price: 8000,
       tags: ['Wedding', 'Portrait'],
       location: 'Greenwich Court',
       reviews: 8,
       rating: 4.5,
-    },
-    {
-      id: 3,
-      image: '/pic.png',
+  },
+  {
+      id: 6,
+      images: ['/pic.png', '/pic.png', '/pic.png'], 
       name: 'John Doe',
       price: 8000,
       tags: ['Wedding', 'Portrait'],
       location: 'Greenwich Court',
       reviews: 8,
       rating: 4.5,
-    },
-    {
-        id: 4,
-        image: '/pic.png',
-        name: 'John Doe',
-        price: 8000,
-        tags: ['Wedding', 'Portrait'],
-        location: 'Greenwich Court',
-        reviews: 8,
-        rating: 4.5,
-      },
-      {
-        id: 5,
-        image: '/pic.png',
-        name: 'John Doe',
-        price: 8000,
-        tags: ['Wedding', 'Portrait'],
-        location: 'Greenwich Court',
-        reviews: 8,
-        rating: 4.5,
-      },
-      {
-        id: 6,
-        image: '/pic.png',
-        name: 'John Doe',
-        price: 8000,
-        tags: ['Wedding', 'Portrait'],
-        location: 'Greenwich Court',
-        reviews: 8,
-        rating: 4.5,
-      },
-  ];
+  },
+];
+
   
   export default function Search() {
     return (   
@@ -72,9 +73,8 @@ const data = [
         <div><CategorySection /></div>
         <div>
           <div><SearchBox /></div>   
-          <div>  <SmallPortfolio data={data} /></div>
+          <div>  <Card data={data} /></div>
         </div>
       </div>    
     );
-
   }
