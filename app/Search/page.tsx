0c33b3658fbe569b44233c1bdb6aee79a1d9732e
Pabodha <127748031+pabodha-0@users.noreply.github.com>
2025-02-17 13:20:01 +0530

    Update page.tsx

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -1,17 +1,18 @@
-import Card from "../components/Search/Card";
 import CategorySection from "../components/Search/CategorySection";
 import SearchBox from "../components/Search/SeacrhBox";
 import styles from "./page.module.css";
 
-
-  
-  export default function Search() {
-    return (   
-      <div className={styles.Container}>
-        <div><CategorySection /></div>
+export default function Search() {
+  return (
+    <div className={styles.Container}>
+      <div>
+        <CategorySection />
+      </div>
+      <div>
         <div>
-          <div><SearchBox /></div>   
+          <SearchBox />
         </div>
-      </div>    
-    );
-  }
+      </div>
+    </div>
+  );
+}
