54df7ea6961d203fc83a7d0b9ed81ee5f024e003
Pamudi <tpamudirajapaksha@gmail.com>
2025-02-20 23:17:48 +0530

    Add a search bar component for booking page

---
+++ app/BookingInformation/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.css"
+import SearchBar from "../components/BookingDetails/SearchBar";
+
+
+const BookingDetails = () => {
+    return(
+        <div>
+            <SearchBar/>
+        </div>
+    )
+}
+
+export default BookingDetails;
