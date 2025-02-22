9aa869c44b94396f4b370d1aa8d7d6dffe571d8d
Pabodha <127748031+pabodha-0@users.noreply.github.com>
2025-02-22 21:20:48 +0530

    Fixed ESlint issues for build

--- app/redux/features/portfolio/index.ts
+++ app/redux/features/portfolio/index.ts
@@ -1,266 +1,225 @@
-import { PortfolioState } from './../../types/portfolio.types';
+import { PortfolioState } from "./../../types/portfolio.types";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios, { AxiosError } from "axios";
-import build from "next/dist/build";
 
 //const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL ;
 //const ROUTE_URL = `${BACKEND_BASE_URL}/portfolio/....`
 
-
-
 const initialState: PortfolioState = {
-    allPortfolio: {
-      isLoading: false,
-      isSuccessful: false,
-      serverPortfolio: "",
-      data: null,
-    },
-    activePortfolio: {
-      isLoading: false,
-      isSuccessful: false,
-      serverPortfolio: "",
-      data:  null,
-      
-    },
-    
-    activeGallery: {
-      isLoading: false,
-      isSuccessful: false,
-      serverPortfolio: "",
-      data: null,
-    },
+  allPortfolio: {
+    isLoading: false,
+    isSuccessful: false,
+    serverPortfolio: "",
+    data: null,
+  },
+  activePortfolio: {
+    isLoading: false,
+    isSuccessful: false,
+    serverPortfolio: "",
+    data: null,
+  },
 
+  activeGallery: {
+    isLoading: false,
+    isSuccessful: false,
+    serverPortfolio: "",
+    data: null,
+  },
 
-    createPortfolio: {
-      isLoading: false,
-      isSuccessful: false,
-      serverPortfolio: "",
-      data: null,
-    },
-  };
+  createPortfolio: {
+    isLoading: false,
+    isSuccessful: false,
+    serverPortfolio: "",
+    data: null,
+  },
+};
 
+export const getAllportfolio = createAsyncThunk(
+  "portfolio/get-all",
+  async ({ participantId }: { participantId: string }, { rejectWithValue }) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: {
+        participantId,
+      },
+    };
 
-  export const getAllportfolio = createAsyncThunk(
-    "portfolio/get-all",
-    async ({ participantId }: { participantId: string }, { rejectWithValue }) => {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        params: {
-          participantId,
-        },
-      };
-  
-      try {
-       // return await axios.get("http://127.0.0.1:8000/api/headers").then((res) => res.data);
-       return await axios.get("http://127.0.0.1:8000/api/headers").then((res) => res.data);
-      } catch (error) {
-        const e = error as AxiosError;
-        return rejectWithValue(e.message);
-      }
+    try {
+      // return await axios.get("http://127.0.0.1:8000/api/headers").then((res) => res.data);
+      return await axios
+        .get("http://127.0.0.1:8000/api/headers", config)
+        .then((res) => res.data);
+    } catch (error) {
+      const e = error as AxiosError;
+      return rejectWithValue(e.message);
     }
-  );
+  }
+);
 
-  export const getByIdportfolio = createAsyncThunk(
+export const getByIdportfolio = createAsyncThunk(
   "portfolio/get-by-id",
-    async ({ participantId }: { participantId: string }, { rejectWithValue }) => {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        params: {
-          participantId,
-        },
-      };
-  
-      try {
+  async ({ participantId }: { participantId: string }, { rejectWithValue }) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: {
+        participantId,
+      },
+    };
 
-       return await axios.get("http://127.0.0.1:8000/api/headers/67ab65b24cb48a7c886d0dfa").then((res) => res.data);
+    try {
+      return await axios
+        .get(
+          "http://127.0.0.1:8000/api/headers/67ab65b24cb48a7c886d0dfa",
+          config
+        )
+        .then((res) => res.data);
 
       //  const [portfolioResponse, galleryResponse] = await Promise.all([
       //   axios.get("http://127.0.0.1:8000/api/headers/67ab65b24cb48a7c886d0dfa"),
       //   axios.get("http://127.0.0.1:8000/api/galleries/67aecc532071993d23e91175"),
       //  ])
-
-
-      
-      } catch (error) {
-        const e = error as AxiosError;
-        return rejectWithValue(e.message);
-      }
+    } catch (error) {
+      const e = error as AxiosError;
+      return rejectWithValue(e.message);
     }
-  );
+  }
+);
 
+export const getByIdgallery = createAsyncThunk(
+  "gallery/get-by-id",
+  async ({ participantId }: { participantId: string }, { rejectWithValue }) => {
+    const config = {
+      galleries: {
+        "Content-Type": "application/json",
+      },
+      params: {
+        participantId,
+      },
+    };
 
+    try {
+      return await axios
+        .get(
+          "http://127.0.0.1:8000/api/galleries/67aecc532071993d23e91175",
+          config
+        )
+        .then((res) => res.data);
 
+      //  const [portfolioResponse, galleryResponse] = await Promise.all([
+      //   axios.get("http://127.0.0.1:8000/api/headers/67ab65b24cb48a7c886d0dfa"),
+      //   axios.get("http://127.0.0.1:8000/api/galleries/67aecc532071993d23e91175"),
+      //  ])
+    } catch (error) {
+      const e = error as AxiosError;
+      return rejectWithValue(e.message);
+    }
+  }
+);
 
+const portfolioSlice = createSlice({
+  name: "portfolio",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Get all portfolio
+    // Pending
+    builder.addCase(getAllportfolio.pending, (state) => {
+      state.allPortfolio = {
+        isLoading: true,
+        isSuccessful: false,
+        serverPortfolio: "",
+        data: null,
+      };
+    });
 
-  export const getByIdgallery = createAsyncThunk(
-    "gallery/get-by-id",
-      async ({ participantId }: { participantId: string }, { rejectWithValue }) => {
-        const config = {
-          galleries: {
-            "Content-Type": "application/json",
-          },
-          params: {
-            participantId,
-          },
-        };
-    
-        try {
-  
-         return await axios.get("http://127.0.0.1:8000/api/galleries/67aecc532071993d23e91175").then((res) => res.data);
-  
-        //  const [portfolioResponse, galleryResponse] = await Promise.all([
-        //   axios.get("http://127.0.0.1:8000/api/headers/67ab65b24cb48a7c886d0dfa"),
-        //   axios.get("http://127.0.0.1:8000/api/galleries/67aecc532071993d23e91175"),
-        //  ])
-  
-  
-        
-        } catch (error) {
-          const e = error as AxiosError;
-          return rejectWithValue(e.message);
-        }
-      }
-    );
+    // Get by id  portfolio
+    // Pending
+    builder.addCase(getByIdportfolio.pending, (state) => {
+      state.activePortfolio = {
+        isLoading: true,
+        isSuccessful: false,
+        serverPortfolio: "",
+        //data : null;
+        data: null,
+      };
+    });
 
+    // Get by id  gallery
+    // Pending
+    builder.addCase(getByIdgallery.pending, (state) => {
+      state.activeGallery = {
+        isLoading: true,
+        isSuccessful: false,
+        serverPortfolio: "",
+        //data : null;
+        data: null,
+      };
+    });
 
+    // Fulfilled
+    builder.addCase(getAllportfolio.fulfilled, (state, action) => {
+      state.allPortfolio = {
+        isLoading: false,
+        isSuccessful: true,
+        serverPortfolio: "",
+        data: action.payload,
+      };
+    });
 
+    // Fulfilled
+    builder.addCase(getByIdportfolio.fulfilled, (state, action) => {
+      state.activePortfolio = {
+        isLoading: false,
+        isSuccessful: true,
+        serverPortfolio: "",
+        data: action.payload,
+      };
+    });
 
+    // Fulfilled
+    builder.addCase(getByIdgallery.fulfilled, (state, action) => {
+      state.activeGallery = {
+        isLoading: false,
+        isSuccessful: true,
+        serverPortfolio: "",
+        data: action.payload,
+      };
+    });
 
+    // Rejected
+    builder.addCase(getAllportfolio.rejected, (state, action) => {
+      state.allPortfolio = {
+        isLoading: false,
+        isSuccessful: false,
+        serverPortfolio: action.payload as string,
+        data: null,
+      };
+    });
 
+    // Rejected
+    builder.addCase(getByIdportfolio.rejected, (state, action) => {
+      state.activePortfolio = {
+        isLoading: false,
+        isSuccessful: false,
+        serverPortfolio: action.payload as string,
+        data: null,
+      };
+    });
 
+    // Rejected
+    builder.addCase(getByIdgallery.rejected, (state, action) => {
+      state.activeGallery = {
+        isLoading: false,
+        isSuccessful: false,
+        serverPortfolio: action.payload as string,
+        data: null,
+      };
+    });
+  },
+});
 
-
-
-
-
-
-
-
-
-
-
-
-  const portfolioSlice = createSlice({
-    name : "portfolio",
-    initialState,
-    reducers : {
-
-    },
-    extraReducers: (builder) => {
-        // Get all portfolio
-        // Pending
-        builder.addCase(getAllportfolio.pending, (state) => {
-        state.allPortfolio = {
-            isLoading: true,
-            isSuccessful: false,
-            serverPortfolio: "",
-            data: null,
-        };
-        });
-
-        // Get by id  portfolio
-        // Pending
-        builder.addCase(getByIdportfolio.pending, (state) => {
-          state.activePortfolio = {
-              isLoading: true,
-              isSuccessful: false,
-              serverPortfolio: "",
-              //data : null;
-              data: null,
-          };
-          });
-
-          
-        
-        // Get by id  gallery
-        // Pending
-        builder.addCase(getByIdgallery.pending, (state) => {
-          state.activeGallery = {
-              isLoading: true,
-              isSuccessful: false,
-              serverPortfolio: "",
-              //data : null;
-              data: null,
-          };
-          });  
-
-
-
-
-
-        // Fulfilled
-            builder.addCase(getAllportfolio.fulfilled, (state, action) => {
-              state.allPortfolio = {
-                isLoading: false,
-                isSuccessful: true,
-                serverPortfolio: "",
-                data: action.payload,
-              };
-            });
-
-
-            // Fulfilled
-            builder.addCase(getByIdportfolio.fulfilled, (state, action) => {
-              state.activePortfolio = {
-                isLoading: false,
-                isSuccessful: true,
-                serverPortfolio: "",
-                data: action.payload,
-              };
-            });
-
-
-            // Fulfilled
-            builder.addCase(getByIdgallery.fulfilled, (state, action) => {
-              state.activeGallery = {
-                isLoading: false,
-                isSuccessful: true,
-                serverPortfolio: "",
-                data: action.payload,
-              };
-            });
-
-
-
-        
-            // Rejected
-            builder.addCase(getAllportfolio.rejected, (state, action) => {
-              state.allPortfolio = {
-                isLoading: false,
-                isSuccessful: false,
-                serverPortfolio: action.payload as string,
-                data: null,
-              };
-            });
-
-            // Rejected
-            builder.addCase(getByIdportfolio.rejected, (state, action) => {
-              state.activePortfolio = {
-                isLoading: false,
-                isSuccessful: false,
-                serverPortfolio: action.payload as string,
-                data:  null,
-              };
-            });
-
-
-            // Rejected
-            builder.addCase(getByIdgallery.rejected, (state, action) => {
-              state.activeGallery = {
-                isLoading: false,
-                isSuccessful: false,
-                serverPortfolio: action.payload as string,
-                data:  null,
-              };
-            });
-
-
-        
-    },
-  });
-
-  export default portfolioSlice.reducer;
+export default portfolioSlice.reducer;
