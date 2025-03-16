08e4f1995e318aa4856fad6f4be5ad923f63dac5
Pabodha <127748031+pabodha-0@users.noreply.github.com>
2025-03-16 22:17:47 +0530

    added navbar auth ui

--- app/components/Layout/Navbar/Desktop/index.tsx
+++ app/components/Layout/Navbar/Desktop/index.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
+import { ChevronDown } from "lucide-react";
+import { LuCalendarRange, LuMessageSquareText, LuUser } from "react-icons/lu";
+import { IoMdLogOut } from "react-icons/io";
 
 const DesktopNavbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
       <Link href="/">
@@ -31,13 +37,54 @@ const DesktopNavbar = () => {
         </Link> */}
       </div>
 
-      <div className={styles.navLinks}>
+      {/* <div className={styles.navLinks}>
         <Link href={"/maintenance"} className="secondary-btn">
           Sign In
         </Link>
         <Link href={"/maintenance"} className="primary-btn">
           Register
         </Link>
+      </div> */}
+
+      <div className={styles.profileWrapper}>
+        <div
+          className={styles.profile}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+          <span>Pabodha</span>
+          <Image
+            src={"/team/pabodha.jpg"}
+            className={styles.avatar}
+            width={40}
+            height={40}
+            alt="User Avatar"
+          />
+          <ChevronDown />
+        </div>
+
+        <ul
+          className={`${styles.dropdown} ${
+            isDropdownOpen ? "" : styles.hidden
+          }`}
+        >
+          <li>
+            <LuUser className={styles.icon} />
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li>
+            <LuCalendarRange className={styles.icon} />
+            <Link href="/profile">Calendar</Link>
+          </li>
+          <li>
+            <LuMessageSquareText className={styles.icon} />
+            <Link href="/profile">Messages</Link>
+          </li>
+          <hr className={styles.hr} />
+          <li className={styles.logout}>
+            <IoMdLogOut className={styles.icon} />
+            <Link href="/profile">Logout</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
